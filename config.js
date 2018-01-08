19ad7ad417518a155a2754a9ea0d717fdd807eb0
Maciej Duda <maciej.duda1@gazeta.pl>
2018-01-08 12:03:15 +0100

    Apka do logowania google

---
+++ config.js
@@ -0,0 +1,5 @@
+﻿module.exports = {
+	'GOOGLE_CLIENT_ID': '983201125370-j9lmgnembkk4f2i48dnembgrdo9kq9t2.apps.googleusercontent.com', 
+	'GOOGLE_CLIENT_SECRET': 'CYTtp3lP4IylKOOLI1lsfsa2',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,35 +1,55 @@
 ﻿var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
-app.use(express.static('assets'));
+var googleProfile = {};
 
-app.use('/store', function(req, res, next){
-	console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
 
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app.use(express.static('assets'));
+
+app.get('/', function(req, res){
+    res.render('index', { user: req.user });
 });
 
-app.get('/store', function(req, res){
-	res.send('To jest sklep')
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
 
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
-
-
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-});
-
-
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
-});
+app.listen(3000);
